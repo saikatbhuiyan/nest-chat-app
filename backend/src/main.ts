beef827299dd5fd51e2b0a1d634113d8f82dacda
saikatbhuiyan <shaikotbhuiya@gmail.com>
2025-03-08 01:14:01 +0600

    Added validations and logger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from 'nestjs-pino';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useGlobalPipes(new ValidationPipe());
+  app.useLogger(app.get(Logger));
+  const configService = app.get(ConfigService);
+  await app.listen(configService.getOrThrow('PORT'));
 }
 bootstrap();
