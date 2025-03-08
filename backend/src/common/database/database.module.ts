00edf4b80bd1c0556eb7d11f796f155a138366c0
saikatbhuiyan <shaikotbhuiya@gmail.com>
2025-03-08 23:24:44 +0600

    Confing migrations

--- backend/src/common/database/database.module.ts
+++ backend/src/common/database/database.module.ts
@@ -1,16 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
+import { DbMigrationService } from './db-migration.service';
 
 @Module({
   imports: [
     MongooseModule.forRootAsync({
       useFactory: (configService: ConfigService) => ({
         uri: configService.get('MONGODB_URI'),
+        //ssl: true, // Enable SSL
       }),
       inject: [ConfigService],
     }),
   ],
+  providers: [DbMigrationService],
 })
 export class DatabaseModule {
   static forFeature(models: ModelDefinition[]) {
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from 'nestjs-pino';
+import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-
-import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
