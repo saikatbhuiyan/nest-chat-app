66fc164e9ae547548b07585dc513f298a7acd7cb
saikatbhuiyan <shaikotbhuiya@gmail.com>
2025-07-02 19:30:56 +0600

    Auth guard for client

--- frontend/src/components/Routes.tsx
+++ frontend/src/components/Routes.tsx
@@ -1,8 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "./auth/Login";
 import Signup from "./auth/Signup";
+import Home from "./home/Home";
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
   {
     path: "/login",
     element: <Login />,
