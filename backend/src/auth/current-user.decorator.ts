b6d800405a0a5764c127d64179178701b98edc6d
saikatbhuiyan <shaikotbhuiya@gmail.com>
2025-07-02 16:40:24 +0600

    Added JwtStrategy and Guards

--- backend/src/auth/current-user.decorator.ts
+++ backend/src/auth/current-user.decorator.ts
@@ -1,9 +1,18 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { GqlContextType, GqlExecutionContext } from '@nestjs/graphql';
 import { User } from 'src/users/entities/user.entity';
 
-const getCurrentUserContext = (context: ExecutionContext): User => {
-  const request = context.switchToHttp().getRequest();
-  return request.user;
+const getCurrentUserContext = (context: ExecutionContext): User | null => {
+  if (context.getType() === 'http') {
+    return (context.switchToHttp().getRequest().user as User) || null;
+  }
+  if (context.getType<GqlContextType>() === 'graphql') {
+    return (
+      (GqlExecutionContext.create(context).getContext().req.user as User) ||
+      null
+    );
+  }
+  return null;
 };
 
 export const CurrentUser = createParamDecorator(
