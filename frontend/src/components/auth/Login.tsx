95437c5801af86bbe66e95543761134b9518fa1c
saikatbhuiyan <shaikotbhuiya@gmail.com>
2025-07-02 18:15:34 +0600

    Login client

--- frontend/src/components/auth/Login.tsx
+++ frontend/src/components/auth/Login.tsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
 import { Link as MUILink } from "@mui/material";
 import Auth from "./Auth";
+import { useLogin } from "../../hooks/useLogin";
 
 function Login() {
+  const { login, error } = useLogin();
+
   return (
-    <Auth submitLabel="Login" handleSubmit={async () => {}}>
+    <Auth submitLabel="Login" handleSubmit={(request) => login(request)}>
       <MUILink
         component={Link}
         to="/signup"
