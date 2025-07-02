f7b824c26f270baad690eb7e53cb8e2b0ce52cac
saikatbhuiyan <shaikotbhuiya@gmail.com>
2025-07-03 00:40:55 +0600

    Auth state for client

--- frontend/src/components/header/Navigation.tsx
+++ frontend/src/components/header/Navigation.tsx
@@ -1,15 +1,21 @@
 import { Box, Button } from "@mui/material";
+import router from "../Routes";
+import { Page } from "../../interfaces/page.interface";
 
 interface NavigationProps {
-  pages: string[];
+  pages: Page[];
 }
 
 const Navigation = ({ pages }: NavigationProps) => {
   return (
     <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
       {pages.map((page) => (
-        <Button key={page} sx={{ my: 2, color: "white", display: "block" }}>
-          {page}
+        <Button
+          key={page.title}
+          onClick={() => router.navigate(page.path)}
+          sx={{ my: 2, color: "white", display: "block" }}
+        >
+          {page.title}
         </Button>
       ))}
     </Box>
