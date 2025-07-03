88a3b2c95d9dc0fafbe4f2cdd1a281563c89af5f
saikatbhuiyan <shaikotbhuiya@gmail.com>
2025-07-03 11:29:56 +0600

    Conditional rendering of chat list

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,6 +13,7 @@ import Guard from "./components/auth/Guard";
 import Header from "./components/header/Header";
 import Snackbar from "./components/snackbar/Snackbar";
 import ChatList from "./components/chat-list/ChatList";
+import { usePath } from "./hooks/usePath";
 
 const darkTheme = createTheme({
   palette: {
@@ -21,26 +22,38 @@ const darkTheme = createTheme({
 });
 
 const App = () => {
+  const { path } = usePath();
+
   return (
     <ApolloProvider client={client}>
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
         <Header />
-        <Box display="flex">
-          <Box flex="0 0 25%" maxWidth="25%">
-            <ChatList />
-          </Box>
-          <Box flex="1">
-            <Container>
-              <Guard>
-                <RouterProvider router={router} />
-              </Guard>
-            </Container>
-          </Box>
-        </Box>
+        <Guard>
+          {path === "/" ? (
+            <Box display="flex">
+              <Box flex="0 0 25%" maxWidth="25%">
+                <ChatList />
+              </Box>
+              <Box flex="1">
+                <Routes />
+              </Box>
+            </Box>
+          ) : (
+            <Routes />
+          )}
+        </Guard>
         <Snackbar />
       </ThemeProvider>
     </ApolloProvider>
+  );
+};
+
+const Routes = () => {
+  return (
+    <Container>
+      <RouterProvider router={router} />
+    </Container>
   );
 };
 
